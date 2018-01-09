53cd2d7ac27c1a77ff038f08eda6a6534ba30303
Regis36 <34751548+Regis36@users.noreply.github.com>
2018-01-09 14:42:33 -0500

    Add files via upload

---
+++ Welcome to my gmail.py
@@ -0,0 +1,35 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+============== RESTART: /Users/regisenama/Documents/Untitled.py ==============
+welcome to Gmail
+Enter your username  and then press return Regis.enama@gmail.com
+Username incorrect
+>>> 
+============== RESTART: /Users/regisenama/Documents/Untitled.py ==============
+welcome to Gmail
+Enter your username  and then press return johndoe
+Username incorrect
+>>> 
+============== RESTART: /Users/regisenama/Documents/Untitled.py ==============
+Welcome to Gmail
+Enter your username, then press Return: johndoe
+Username is correct
+Enter your password, then press return: sof123@
+Success
+>>> 
+============== RESTART: /Users/regisenama/Documents/Untitled.py ==============
+Welcome to Gmail
+Enter your username, then press Return: johndoe
+Username is correct
+Enter your password, then press return: regis
+Unable to log in. Inocrrect password
+>>> 
+============== RESTART: /Users/regisenama/Documents/Untitled.py ==============
+Welcome to Gmail
+Enter your username, then press Return: Regisenama
+Incorrect. Try again
+>>> 
